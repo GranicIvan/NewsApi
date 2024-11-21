8aee505bffbdef36d7d67bf3d38ea61ae2597a09
GranicIvan <granic.ivan.01@gmail.com>
2024-11-21 23:26:41 +0100

    Add project files.

---
+++ NewsApi.Data/Base/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsApi.Model.Models;
+
+namespace NewsApi.Data.Base
+{
+    internal class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        public DbSet<NewsArticle> NewsArticles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ NewsApi.Model/Models/NewsArticle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApi.Model.Models
+{
+    public class NewsArticle
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public List<Tag> Tags { get; set; }
+        
+        public Category Category { get; set; }//Key
+        public string ImageUrl { get; set; }
+
+    }
+}
